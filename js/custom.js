a593456a22930a3450a30931a63fdb67bb01a720
adrian cheung <92572031+adriancheung111@users.noreply.github.com>
2025-02-21 01:07:16 +0800

    ver1

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,39 @@
 // to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
+// function getYear() {
+//     var currentDate = new Date();
+//     var currentYear = currentDate.getFullYear();
+//     document.querySelector("#displayYear").innerHTML = currentYear;
+// }
 
-getYear();
+// getYear();
+$(document).ready(function () {
+  // 使用 jQuery 的 load 方法來加載 footer.html 並將其插入到頁面
+  $("#footer-container").load("footer.html");
+});
+
+$(document).ready(function () {
+  // 使用 jQuery 的 load 方法來加載 footer.html 並將其插入到頁面
+  $("#nav-container").load("nav.html");
+});
 
 // overlay menu
 function openNav() {
-    document.getElementById("myNav").classList.toggle("menu_width");
-    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
+  document.getElementById("myNav").classList.toggle("menu_width");
+  document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
 }
-
 
 /** google_map js **/
 
 function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  var mapProp = {
+    center: new google.maps.LatLng(40.712775, -74.005973),
+    zoom: 18,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 
 // lightbox gallery
 $(document).on("click", '[data-toggle="lightbox"]', function (event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
+  event.preventDefault();
+  $(this).ekkoLightbox();
 });
